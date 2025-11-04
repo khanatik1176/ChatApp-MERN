30db592e8fe09198ce0b794013f6e449440a3089
Khan Atik Faisal <khanatik1176@gmail.com>
2025-11-04 17:07:55 +0600

    Added-till-updateProfile-backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,16 +2,19 @@ import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import dotenv from 'dotenv';
 import { connectDB } from './lib/db.js';
-const app = express();
+import cookieParser from 'cookie-parser';
 
-dotenv.config();
+const app = express(); // Create an Express application instance
+
+dotenv.config(); // Load environment variables from .env file
 const PORT = process.env.PORT || 5001;
 
-app.use("/api/auth", authRoutes);
-
+app.use(express.json());
+app.use(cookieParser()); // This will extract JSON data from incoming requests
+app.use("/api/auth", authRoutes); // Mount the auth routes at /api/auth
 
 app.listen(PORT, () => 
 {
     console.log('Server is running on port:', PORT);
-    connectDB();
+    connectDB(); // Connect to the database when the server starts
 })
