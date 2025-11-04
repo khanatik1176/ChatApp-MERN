8c14d49331756e275245f96460157c7b7dc80f30
Khan Atik Faisal <khanatik1176@gmail.com>
2025-11-04 15:55:20 +0600

    db-added

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export const signup = (req, res) => 
+{
+    res.send('Signup Route');
+}
+
+export const login = (req, res) => 
+{
+    res.send('Login Route');
+}
+
+export const logout = (req, res) => 
+{
+    res.send('Logout Route');
+}
